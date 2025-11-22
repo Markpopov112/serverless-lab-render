b3dc815b75540b2ed1aaf59077c95cd8757d029f
Mark 100 <popmark31122003@gmail.com>
2025-11-22 03:43:34 +0300

    ADD: echo endpoint finally added

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, Serverless! 🚀\n", 200, {'Content-Type': 'text/plain'}
+    return "Hello, Serverless! Working\n", 200, {'Content-Type': 'text/plain'}
 
-# Этот блок только для локального запуска
+@app.route('/echo', methods=['POST'])
+def echo():
+    return jsonify({"status": "echo endpoint works"})
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
